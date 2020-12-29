87b392072c01ec7082cb1bd5f02ad63794772b90
OmaimaSalem <omaimasalem933@gmail.com>
2020-12-29 22:51:45 +0200

    Angular Lab3

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {SearchService} from'../../serves/search.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   title = 'BookFinder';
+  data: any;
+  name: any;
+  constructor(private userservice: SearchService){}
+  ngOnInit(): void {
+    
+  }
+  
+  handlesearch(){
+    this.name=document.getElementById('user');
+    this.name=this.name.value;
+    this.userservice.setParram(this.name);
+    
+    this.userservice.searchList().subscribe({
+      next: (data) => {
+        data['items'].forEach(element => {
+          console.log(element.volumeInfo.title);
+        });
+        this.data=data['items']; 
+      },
+      error: (msg) => {
+        console.log('error',msg);
+      }
+    })
+    
+  }
+}
+
